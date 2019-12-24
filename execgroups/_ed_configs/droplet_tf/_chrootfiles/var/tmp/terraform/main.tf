f42dec609e0c4a5dc5357f9081806d647525878c
Gary <gary@elasticdev.io>
2019-12-24 12:48:52 +0800

    update

--- execgroups/_ed_configs/droplet_tf/_chrootfiles/var/tmp/terraform/main.tf
+++ execgroups/_ed_configs/droplet_tf/_chrootfiles/var/tmp/terraform/main.tf
@@ -5,7 +5,7 @@ provider "digitalocean" {
 resource "digitalocean_droplet" "vm" {
   count = "${var.vm_num_of_droplets}"
   ssh_keys           = ["${var.vm_ssh_key_id}"]
-  image              = "${var.vm_image}"
+  image              = "ubuntu-18-04-x64"
   region             = "${var.vm_region}"
   size               = "${var.vm_size}"
   private_networking = "${var.vm_private_networking}"
